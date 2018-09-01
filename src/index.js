72c03c551bb8bd1bd6b18cd0ef450dce10e963a4
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-09-01 11:22:47 -0300

    first setup webpack and react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => {
+    return <div>Hola Mundo!</div>
+}
+
+ReactDOM.render(<Index />, document.querySelector('#index'))
